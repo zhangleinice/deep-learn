b5a864ad9cc8fc1288b833b7717b848e2826ea85
rocky <963754365@qq.com>
2023-09-26 17:52:20 +0800

    feat: 整理目录

--- 04-learn/train_neuralnet.py
+++ 04-learn/train_neuralnet.py
@@ -5,6 +5,7 @@ from data.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 import matplotlib.pyplot as plt
 
+
 # 读入数据
 (x_train, t_train), (x_test, t_test) = load_mnist(
     normalize=True, one_hot_label=True)
